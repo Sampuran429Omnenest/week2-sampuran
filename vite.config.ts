a9959f92de148a74f3a4f171cea569aecb57dfaf
Sampuran429Omnenest <sampuran.udeshi@omnenest.com>
2026-02-23 15:06:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:'/week2-sampuran/',
+  resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 })
